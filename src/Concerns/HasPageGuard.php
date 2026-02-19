019ac65b23aa2e6ad53ebaafb6295c1088784454
Lubos Babocky <Babocky.Lubos@dpb.sk>
2026-02-19 11:25:30 +0100

    add method shouldRegisterNavigation to HasPageGuard trait

--- src/Concerns/HasPageGuard.php
+++ src/Concerns/HasPageGuard.php
@@ -9,20 +9,25 @@ trait HasPageGuard
 {
     public static function canAccess(): bool
     {
-        return Gate::allows(self::getAccessPermission());
+        return Gate::allows(ability: self::getAccessPermission());
+    }
+
+    public static function shouldRegisterNavigation(): bool
+    {
+        return Gate::allows(ability: self::getAccessPermission());
     }
 
     public static function getAccessPermission(): string
     {
-        $fqcnParts = explode('\\', self::class);
-        if (str_starts_with($fqcnParts[1], $fqcnParts[0])) {
-            $fqcnParts[1] = str_replace($fqcnParts[0], '', $fqcnParts[1]);
+        $fqcnParts = explode(separator: '\\', string: self::class);
+        if (str_starts_with(haystack: $fqcnParts[1], needle: $fqcnParts[0])) {
+            $fqcnParts[1] = str_replace(search: $fqcnParts[0], replace: '', subject: $fqcnParts[1]);
         }
         return sprintf(
             '%s-%s.page-access.%s',
-            Str::kebab($fqcnParts[0]),
-            Str::kebab($fqcnParts[1]),
-            Str::kebab(end($fqcnParts))
+            Str::kebab(value: $fqcnParts[0]),
+            Str::kebab(value: $fqcnParts[1]),
+            Str::kebab(value: end(array: $fqcnParts))
         );
     }
 }
